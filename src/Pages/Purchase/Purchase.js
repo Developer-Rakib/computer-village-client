c0246078ce50c032ec8039e17f9c67149bc65222
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-05-24 00:15:05 +0600

    purchase page done

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import toast from 'react-hot-toast';
 import { MdOutlineAddCircle } from 'react-icons/md';
@@ -10,18 +10,33 @@ import Loader from '../Shared/Loader';
 import PurchaseModal from './PurchaseModal';
 
 const Purchase = () => {
+    const [partDetail, setPartDetail] = useState(null)
+    const [part, setPart] = useState({})
+    const [quanity, setQuantity] = useState(null)
+    const [quanityError, setQuantityError] = useState(null)
+    console.log(part?.minimumOrderQuanity);
+    const [loadingg, setLoadingg] = useState(true)
     const { id } = useParams()
     const [user, loading] = useAuthState(auth);
-    const [partDetail, setPartDetail] = useState(null)
-    const { isLoading, error, data: part, refetch } = useQuery('part', () =>
-        axios.get(`http://localhost:5000/part/${id}`)
-    )
 
-    if (loading || isLoading) {
+    useEffect(() => {
+        axios.get(`http://localhost:5000/part/${id}`)
+            .then(data => {
+                console.log(data.data.minimumOrderQuanity);
+                setPart(data.data)
+                setQuantity(data.data.quanity)
+                setLoadingg(false)
+            })
+    }, [id])
+
+
+    if (loading || loadingg) {
         return <Loader></Loader>
     }
-    const {img, name, price, minimumOrderQuanity, availableQuanity} = part?.data;
-    const handlePurchase = (event) =>{
+
+    const { img, name, price, minimumOrderQuanity, availableQuanity } = part;
+    // console.log(part);
+    const handlePurchase = (event) => {
         event.preventDefault();
         const quanity = event.target.quanity.value;
         if (quanity < minimumOrderQuanity) {
@@ -34,10 +49,10 @@ const Purchase = () => {
         }
         const unitPrice = quanity * price;
         const purchaseInfo = {
-            customerName : user.displayName,
-            customerEmail : user.email,
-            customerPhone : event.target.phone.value,
-            customerAddress : event.target.address.value,
+            customerName: user.displayName,
+            customerEmail: user.email,
+            customerPhone: event.target.phone.value,
+            customerAddress: event.target.address.value,
             unitPrice,
             price,
             quanity,
@@ -45,6 +60,20 @@ const Purchase = () => {
         }
         console.log(purchaseInfo);
     }
+    const handleQuantity = (e) => {
+        setQuantity(e.target.value)
+        if (e.target.value < minimumOrderQuanity) {
+            setQuantityError(`You Have to Purchase Atlest ${minimumOrderQuanity} Products`)
+            return
+        }
+        if (e.target.value > availableQuanity) {
+            setQuantityError('Please Order Less Then Available Stock!')
+            return
+        }
+        setQuantityError(null)
+    }
+
+console.log(quanityError);
     return (
         <div
             style={{ fontFamily: 'Merriweather' }}
@@ -82,58 +111,35 @@ const Purchase = () => {
                                 <label for="floating_phone" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-primary peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number</label>
                             </div>
                             <div class="relative z-0 w-full mb-6 group">
-                                <input type="number" name="quanity" id="floating_company" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-primary peer" placeholder=" " required="" />
+                                <input onChange={handleQuantity} type="number" name="quanity" id="floating_company" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-primary peer" placeholder=" " required="" value={quanity == null ? minimumOrderQuanity : quanity} />
+                                <label className="label">
+                                    { quanityError && <span className="label-text-alt text-red-500">{quanityError}</span>}
+                                </label>
                                 <label for="floating_company" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-primary peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Qunatity</label>
+
                             </div>
+
                         </div>
                         <div className='flex justify-center'>
                             <button
-                                // disabled={part.length === 0}
-                                type='submit' style={{ fontFamily: 'Open Sans, sans-serif', letterSpacing: '2px' }} class="hover:bg-white  bg-primary mr-2 transition hover:text-primary rounded-full text-white border-2 border-primary px-10 py-2">Purchase</button>
+                                disabled={(quanity < minimumOrderQuanity || quanity > availableQuanity) && true}
+                                type='submit' style={{ fontFamily: 'Open Sans, sans-serif', letterSpacing: '2px' }} class={(quanity < minimumOrderQuanity || quanity > availableQuanity) ? `bg-gray-400 text-white rounded-full px-10 py-2 mr-2` : `hover:bg-white  bg-primary mr-2 transition hover:text-primary rounded-full text-white border-2 border-primary px-10 py-2`}>Purchase</button>
 
                             <label
-                                onClick={() => setPartDetail(part.data)}
+                                onClick={() => setPartDetail(true)}
                                 for="purchase-modal"
                                 style={{ fontFamily: 'Open Sans, sans-serif', letterSpacing: '2px' }} class="hover:bg-primary transition hover:text-white rounded-full text-primary border-2 border-primary px-10 py-2">Details</label>
                         </div>
                     </form>
                     {
                         partDetail && <PurchaseModal
-                            part={part.data}
+                            part={part}
                         ></PurchaseModal>
                     }
 
                 </div>
 
             </div>
-
-
-
-
-
-
-
-            {/* <div
-             className="flex justify-evenly flex-col-reverse sm:flex-row pt-8 sm:pt-20 w-11/12 sm:container mx-auto">
-                    <div className="flex flex-col p-2 sm:p-5 sm:text-left text-center sm:mt-0 mt-3 md:flex-row md:w-8/12  rounded-lg bg-white shadow-lg">
-                        <img className=" w-52 sm:mx-0 mx-auto sm:w-full  sm:h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg" src={'https://www.computervillage.com.bd/uploads/products/photos/item_Deepcool-Castle-240RGB-V2-Liquid-CPU-Cooler-__1649832634.webp'} alt="" />
-                        <div className="py-6 pl-10  flex flex-col justify-start">
-                            <h5 className="text-gray-900 text-3xl font-semibold mb-2">{'Gigabyte Geforce RTX 2060 SUPER WINDFORCE OC 8GB Graphics Card'}</h5>
-                            <h3 className="text-2xl mb-2 font-semibold">${500}</h3>
-                            <p 
-                            style={{fontFamily:'Open Sans, sans-serif', letterSpacing:'1px'}} 
-                            className="text-gray-700 text-sm pl-1 text-justify sm:text-left sm:text-base mb-2 sm:mb-4">{'The Deepcool Gamer Storm Castle 240RGB V2 Liquid CPU Cooler features an Anti-leak Tech Inside the liquid cooling system. The technology assists the system in achieving automated pressure balance, which significantly improves the operational safety of AIO liquid cooling systems. It has double window panes and immersive ambient lighting,'}</p>
-                            <p> <strong>Stock</strong> : {20}</p>
-                            <p> <strong>Minimum Order</strong> : {12}</p>
-                            <div className='flex justify-between items-center mt-3 sm:pr-3'>
-                            <button style={{fontFamily:'Open Sans, sans-serif', letterSpacing:'2px'}} class="hover:bg-white  bg-primary transition hover:text-primary rounded-full text-white border-2 border-primary px-10 py-2">Purchase</button>
-                            </div>
-
-                        </div>
-                    </div>
-                    
-            </div> */}
-
         </div>
     );
 };
